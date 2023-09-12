114e509c5f4bbe3e1d0ffc70dbd61b673357b42e
guimotab <guimota22@gmail.com>
2023-09-11 21:48:42 -0400

    atualizando erros nos responsivos

--- dist/js/utils/abreAside.ts
+++ dist/js/utils/abreAside.ts
@@ -4,11 +4,11 @@ export default function abreAside() {
     const closeAside = document.getElementById("close-aside")!
 
     menuHambuguer.addEventListener("click", event => {
-        aside.classList.remove("flex")
-    aside.classList.add("hidden") 
-    }) 
-    closeAside.addEventListener("click", event => {
         aside.classList.remove("hidden")
         aside.classList.add("flex")
+    })
+    closeAside.addEventListener("click", event => {
+        aside.classList.remove("flex")
+        aside.classList.add("hidden")
     })
 }
--- public/js/utils/abreAside.js
+++ public/js/utils/abreAside.js
@@ -3,11 +3,11 @@ export default function abreAside() {
     const menuHambuguer = document.getElementById("menu-hamburguer");
     const closeAside = document.getElementById("close-aside");
     menuHambuguer.addEventListener("click", event => {
-        aside.classList.remove("flex");
-        aside.classList.add("hidden");
-    });
-    closeAside.addEventListener("click", event => {
         aside.classList.remove("hidden");
         aside.classList.add("flex");
+    });
+    closeAside.addEventListener("click", event => {
+        aside.classList.remove("flex");
+        aside.classList.add("hidden");
     });
 }
